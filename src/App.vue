53f78e2ec3a5a5ebef34602eb2954d5f6d1acfb4
StefaniaSperani <thedarkcry@hotmail.it>
2023-03-13 14:13:48 +0100

    add bg change

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="weather.main != 'undefined' && weather.main.temp > 15 ? 'warm' : ''">
     <main>
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..." v-model="query" @keypress="fetchWeather">
@@ -75,6 +75,10 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+
+#app .warm {
+  background-image: url(./assets/warm-bg.jpg);
 }
 
 main {
